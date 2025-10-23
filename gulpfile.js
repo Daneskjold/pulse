760e4d87bce991d1aef18678ea1de26434f577a3
Daniel <bazorov.dany@gmail.com>
2025-10-23 14:06:43 +0300

    optimized html, css, images + edited gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
-const {src, dest, watch, parallel} = require('gulp');
+const {src, dest, watch, parallel, series} = require('gulp');
 
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer').default;
 const rename = require('gulp-rename');
+const clean = require('gulp-clean');
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
+
 
 
 function styles() {
@@ -18,21 +22,48 @@ function styles() {
 }
 
 function watching() {
+    browserSync.init({
+        server: {
+            baseDir: "dist/"
+        }
+    });
     watch(['src/sass/**/*.scss'], styles)
+    watch(['src/**/*.html']).on('change', parallel(html))
     watch(['src/**/*.html']).on('change', browserSync.reload)
     watch(['src/js/*.js']).on('change', browserSync.reload);
 }
 
-function browsersync() {
-    browserSync.init({
-        server: {
-            baseDir: "src/"
-        }
-    });
+function html() {
+    return src('src/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(dest('dist/'))
+}
+
+function cleanDist() {
+    return src('dist')
+        .pipe(clean());
+}
+
+function building() {
+    return src([
+        'src/css/style.min.css',
+        'src/js/**/*.js',
+        'src/fonts/*',
+        'src/icons/**/*'
+    ], {base : 'src', encoding: false})
+    .pipe(dest('dist'));
+}
+
+function images() {
+    return src('src/img/**/*.+(png|jpg|svg)', {base : 'src', encoding: false})
+        .pipe(imagemin())
+        .pipe(dest('dist/'))
 }
 
 exports.styles = styles;
 exports.watching = watching;
-exports.browsersync = browsersync;
+exports.building = building;
+exports.images = images;
 
-exports.default = parallel(styles, watching, browsersync);
+exports.build = series(cleanDist, building)
+exports.default = parallel(styles, watching);
